fa1a76510355cc45507714b79c50936a961ef860
xxufji <144124889+xxufji@users.noreply.github.com>
2024-05-02 09:30:17 +0300

    Add files via upload

---
+++ 7дкр/Fractal.pas
@@ -0,0 +1,24 @@
+﻿uses GraphABC, HilbertCurve; 
+ 
+procedure KeyDown(key: Integer); 
+begin 
+ case key of 
+ VK_W: Inc(depth); //Кнопка W - увеличить глубину
+ VK_S: if depth > 1 then Dec(depth); //Кнопка S - уменьшить глубину
+ VK_LEFT: Dec(xOffset, 10); 
+ VK_RIGHT: Inc(xOffset, 10); 
+ VK_UP: Dec(yOffset, 10); 
+ VK_DOWN: Inc(yOffset, 10); 
+ VK_A: scaleFactor := scaleFactor * 1.1; // Кнопка A - увеличиваем масштаб 
+ VK_D: scaleFactor := scaleFactor / 1.1; // Кнопка D - уменьшаем масштаб 
+ end; 
+ DrawFractal; 
+end; 
+
+begin 
+ SetWindowCaption('Фракталы: Кривая Гильберта'); 
+ SetWindowSize(500, 500); 
+ ClearWindow; 
+ OnKeyDown := KeyDown; 
+ DrawFractal; 
+end.
---
+++ 7дкр/HilbertCurve.pas
@@ -0,0 +1,81 @@
+﻿Unit HilbertCurve;
+uses GraphABC;
+
+const 
+ u = 10; 
+ p = 1;
+
+var 
+ scaleFactor: Real = 1.0; 
+ depth: Integer = p; 
+ xOffset: Integer = 100; 
+ yOffset: Integer = 100; 
+ 
+procedure LineRel(dx, dy: Integer); 
+begin 
+ LineTo(PenX + dx, PenY + dy); 
+end; 
+procedure a(i: Integer; scale: Real); forward; 
+procedure b(i: Integer; scale: Real); forward; 
+procedure c(i: Integer; scale: Real); forward; 
+procedure d(i: Integer; scale: Real); forward; 
+procedure a(i: Integer; scale: Real); 
+begin 
+ if i > 0 then 
+ begin 
+ d(i - 1, scale); 
+ LineRel(Round(u * scale), 0); 
+ a(i - 1, scale); 
+ LineRel(0, Round(u * scale)); 
+ a(i - 1, scale); 
+ LineRel(-Round(u * scale), 0); 
+ c(i - 1, scale); 
+ end; 
+end; 
+procedure b(i: Integer; scale: Real); 
+begin 
+ if i > 0 then 
+ begin 
+ c(i - 1, scale); 
+ LineRel(-Round(u * scale), 0); 
+ b(i - 1, scale); 
+ LineRel(0, -Round(u * scale)); 
+ b(i - 1, scale); 
+ LineRel(Round(u * scale), 0); 
+ d(i - 1, scale); 
+ end; 
+end; 
+procedure c(i: Integer; scale: Real); 
+begin 
+ if i > 0 then 
+ begin 
+ b(i - 1, scale); 
+ LineRel(0, -Round(u * scale)); 
+ c(i - 1, scale); 
+ LineRel(-Round(u * scale), 0); 
+ c(i - 1, scale); 
+ LineRel(0, Round(u * scale)); 
+ a(i - 1, scale); 
+ end; 
+end; 
+procedure d(i: Integer; scale: Real); 
+begin 
+ if i > 0 then 
+ begin 
+ a(i - 1, scale); 
+ LineRel(0, Round(u * scale)); 
+ d(i - 1, scale); 
+ LineRel(Round(u * scale), 0); 
+ d(i - 1, scale); 
+ LineRel(0, -Round(u * scale)); 
+ b(i - 1, scale); 
+ end; 
+end; 
+procedure DrawFractal; 
+begin 
+ ClearWindow; 
+ MoveTo(xOffset, yOffset); 
+ a(depth, scaleFactor); 
+end; 
+begin
+end.
